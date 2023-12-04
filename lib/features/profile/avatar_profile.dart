9945fd3c3a79470fae9937570f406cc88632ceec
nuansarahardian <nuansa.rahardian@mhs.unsoed.ac.id>
2023-12-05 00:21:18 +0700

    tambahan avatar profile

--- lib/features/profile/avatar_profile.dart
+++ lib/features/profile/avatar_profile.dart
@@ -2,16 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:iconify_flutter/iconify_flutter.dart';
 import 'package:iconify_flutter/icons/bx.dart';
-import 'package:unsoedfess/provider/user_provider.dart';
 
 class AvatarProfile extends ConsumerWidget {
   final double radius;
-  const AvatarProfile({super.key, this.radius = 50});
+  final String avatarUrl;
+  const AvatarProfile({super.key, this.radius = 50, required this.avatarUrl});
 
   @override
   Widget build(BuildContext context, ref) {
-    final avatarUrl = ref.read(userProvider).profile?.avatar;
-    return avatarUrl!.isEmpty
+    return avatarUrl.isEmpty
         ? AvatarPlaceholder(radius: radius)
         : CircleAvatar(radius: radius, backgroundImage: NetworkImage(avatarUrl));
   }
